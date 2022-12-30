30280a602cd87ceac21eafa1bc067c58530848b7
TusharGaur <gaurtushar5567@gmail.com>
2022-12-31 00:29:39 +0530

    some changes in the app.js file

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-let productInputElement =document.getElementById('product-input');
-let remainingChar = document.getElementById('charCount');
+let productInputElement = document.getElementById('product-input');
+//console.log(productInputElement);
+let remainingChar = window.document.getElementById('charCount');
 
 let maxAllowedChar = productInputElement.maxLength;
+//console.log(maxAllowedChar);
 function updateReaminingChar(event){
     let enteredText =event.target.value;
     let enteredTextLength = enteredText.length;
+    // let elementId = productInputElement;
+    // console.log(elementId);
 
     let remainingCharacter =60- enteredTextLength;
     remainingChar.textContent = remainingCharacter;
+
+    if(remainingCharacter <= 10){
+       
+       // console.dir(productInputElement);
+         remainingChar.classList.add('warning');
+         productInputElement.classList.add("warning");
+        // console.log(remainingCharacter);
+    }
+     else{
+    
+         remainingChar.classList.remove('warning');
+    //     productInputElement.classList.remove('warning');
+     }
 }
 productInputElement = addEventListener('input', updateReaminingChar);
